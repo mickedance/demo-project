64f9e5fff54adebfa9184baa58c08fee146c01f2
mickedance <mickedance@hotmail.com>
2022-03-21 10:48:15 +0000

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int x= 10;
+
+        String hello = "hello";
+        hello += " ehe";
+        System.out.println("x is" +  x + ", "+ hello);
+        System.out.println( "Hello World!" );
+    }
+}
